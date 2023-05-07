7e1a295c521450e5abe94b632927ee7e306030ce
ZzuuCkk <zura.dzidzaria@gmail.com>
2023-05-07 21:34:11 +0400

    first section done

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll(".slide");
+let setIntervalId = null;
+let activeIndex = 0;
+function renderSlides() {
+	slides.forEach((el, i) => {
+		if (i === activeIndex) {
+			el.classList.add("active");
+		} else {
+			el.classList.remove("active");
+		}
+	});   
+}
+function mainSlider(){
+
+function nextFun() {
+    if (activeIndex === slides.length - 1) {
+        activeIndex = 0;
+    } else {
+        activeIndex++;
+    }
+
+    renderSlides();
+}
+
+
+    function startInterval() {
+        slideIntervalId = setInterval(nextFun, 5000);
+    }
+
+    function stopInterval() {
+        clearInterval(slideIntervalId);
+    }
+startInterval();
+    slider.addEventListener("mouseenter", stopInterval);
+    slider.addEventListener("mouseleave", startInterval);
+
+
+    renderSlides();
+}
+
+mainSlider();
