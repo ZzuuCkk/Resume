76a7abfd3b00b29c96d996176842e7ef71c3db59
ZzuuCkk <zura.dzidzaria@gmail.com>
2023-05-09 15:48:04 +0400

    ////

--- assets/js/service.js
+++ assets/js/service.js
@@ -1,32 +1,32 @@
-const services = [
-    {
-      title: "Web Design",
-      description: "I create beautiful and functional websites that help your business grow. I work with you to understand your needs and goals, and I design a website that is tailored to your business.",
-      icon: "./assets/icons/design.svg"
-    },
-    {
-      title: "Pixel Perfect",
-      description: "I pay attention to every detail to ensure that your website looks perfect on all devices. From the layout to the typography, I make sure that every element is pixel-perfect.",
-      icon: "./assets/icons/pixel.svg"
-    },
-    {
-      title: "Unique Ideas",
-      description: "I believe that every business is unique, and I strive to create websites that reflect that. I come up with unique ideas that will make your website stand out from the competition.",
-      icon: "./assets/icons/ideas.svg"
-    },
-    {
-      title: "Helpful Support",
-      description: "I provide helpful and responsive support to ensure that your website is always up and running smoothly. Whether you need help with updates, maintenance, or troubleshooting, I am here to help.",
-      icon: "./assets/icons/support.svg"
-    },
-    {
-      title: "Design Analysis",
-      description: "I analyze your website design to identify areas for improvement and optimization. I provide you with actionable insights that will help you improve your website's performance and user experience.",
-      icon: "./assets/icons/analysis.svg"
-    },
-    {
-      title: "Best Service",
-      description: "I am committed to providing the best service to my clients. I work closely with you to understand your needs and goals, and I strive to exceed your expectations in every way.",
-      icon: "./assets/icons/service.svg"
-    }
-  ];
+// const services = [
+//     {
+//       title: "Web Design",
+//       description: "I create beautiful and functional websites that help your business grow. I work with you to understand your needs and goals, and I design a website that is tailored to your business.",
+//       icon: "./assets/icons/design.svg"
+//     },
+//     {
+//       title: "Pixel Perfect",
+//       description: "I pay attention to every detail to ensure that your website looks perfect on all devices. From the layout to the typography, I make sure that every element is pixel-perfect.",
+//       icon: "./assets/icons/pixel.svg"
+//     },
+//     {
+//       title: "Unique Ideas",
+//       description: "I believe that every business is unique, and I strive to create websites that reflect that. I come up with unique ideas that will make your website stand out from the competition.",
+//       icon: "./assets/icons/ideas.svg"
+//     },
+//     {
+//       title: "Helpful Support",
+//       description: "I provide helpful and responsive support to ensure that your website is always up and running smoothly. Whether you need help with updates, maintenance, or troubleshooting, I am here to help.",
+//       icon: "./assets/icons/support.svg"
+//     },
+//     {
+//       title: "Design Analysis",
+//       description: "I analyze your website design to identify areas for improvement and optimization. I provide you with actionable insights that will help you improve your website's performance and user experience.",
+//       icon: "./assets/icons/analysis.svg"
+//     },
+//     {
+//       title: "Best Service",
+//       description: "I am committed to providing the best service to my clients. I work closely with you to understand your needs and goals, and I strive to exceed your expectations in every way.",
+//       icon: "./assets/icons/service.svg"
+//     }
+//   ];
